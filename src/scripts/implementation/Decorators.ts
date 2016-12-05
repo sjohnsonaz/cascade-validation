f168758e394be80ddbb23487b2d2feb226a53c3a
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-12-05 10:31:11 -0700

    Fixed required decorator.

--- src/scripts/implementation/Decorators.ts
+++ src/scripts/implementation/Decorators.ts
@@ -1,18 +1,21 @@
+import Cascade from 'cascade';
+
 import Validation from './Validation';
 import Rule from './Rule';
 
 export function required<T>(target: any, propertyKey: string, descriptor?: TypedPropertyDescriptor<T>): any {
-    Object.defineProperty(target, propertyKey + '-valid', {
+    var validPropertyKey = propertyKey + '-valid';
+    Object.defineProperty(target, validPropertyKey, {
         enumerable: true,
         configurable: true,
         get: function() {
-            Validation.attachGraph(target);
-            if (!target._validation.rules[propertyKey]) {
-                Validation.createRule(target, propertyKey, new Rule(() => {
+            Cascade.attachGraph(this);
+            if (!this._graph.observables[validPropertyKey]) {
+                this._graph.observables[validPropertyKey] = new Rule(() => {
                     return !!this[propertyKey];
-                }));
+                });
             }
-            return target._validation.rules[propertyKey].getValue();
+            return this._graph.observables[validPropertyKey].getValue();
         }
     });
 }
