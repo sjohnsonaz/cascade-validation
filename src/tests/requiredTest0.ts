dcb15c5f5ba1758fcdc9179438e6706b914bac6e
Sean Johnson <sjohnson@sjohnsonaz.com>
2016-12-05 17:02:02 -0700

    Added more required tests.

--- src/tests/requiredTest0.ts
+++ src/tests/requiredTest0.ts
@@ -4,13 +4,27 @@ import {observable} from 'cascade';
 import {required} from '../scripts/CascadeValidation';
 
 describe('required', () => {
-    it('should observe model properties', () => {
-        class Model {
-            @required @observable value: string = 'abcd';
-        }
+    class Model {
+        @required @observable value: string;
+    }
 
+    it('should be false with default values', () => {
         var model = new Model();
         var valid = model['value-valid'];
+        expect(valid).to.equal(false);
+    });
+
+    it('should be true if property is truthy', () => {
+        var model = new Model();
+        model.value = 'abcd';
+        var valid = model['value-valid'];
         expect(valid).to.equal(true);
+    });
+
+    it('should be false if property is falsy', () => {
+        var model = new Model();
+        model.value = '';
+        var valid = model['value-valid'];
+        expect(valid).to.equal(false);
     });
 });
